03d61c0e02307f646b19cb29b5405eff924c58a6
takeda-chiaki-53b <toylgmop@gmail.com>
2024-08-17 20:00:38 +0900

    トップページ(仮)の作成 #9

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 不要なファイルを生成しない設定
+    config.generators do |g|
+      g.helper false # helper ファイルを作成しない
+      g.test_framework false # test ファイルを作成しない
+      g.skip_routes true # ルーティングの記述を作成しない
+    end
   end
 end
