c98a7f18924450e6e349c3b95fe1c97febfa17b7
takeda-chiaki-53b <toylgmop@gmail.com>
2024-09-01 19:24:49 +0900

    エラー内容表示順をフォームに合わせるためのバリデーション行入れ替え

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, uniqueness: true
 
   # 権限判定用
   enum role: { general: 0, brand_admin: 1, app_admin: 2 }
