9f6715850dab27c73e7d88de8a2ff44b615d1be7
takeda-chiaki-53b <toylgmop@gmail.com>
2024-09-12 03:09:53 +0900

    ブランドアカウントのみがアクセス可能な状態に制限 #33

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,20 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   before_action :require_login
+  before_action :check_brand_admin, if: :brand_admin_controller?
   add_flash_types :success, :danger
 
   private
 
   def not_authenticated
     redirect_to login_path
+  end
+
+  def check_brand_admin
+    redirect_to root_path, danger: "権限がありません" unless current_user&.brand_admin?
+  end
+
+  def brand_admin_controller?
+    controller_path.start_with?('brand_admin/')
   end
 end
