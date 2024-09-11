e6d0385d9094581f4a52069c962e03283573a298
takeda-chiaki-53b <toylgmop@gmail.com>
2024-09-11 21:23:43 +0900

    ブランド管理者用マイページのルーティングと 関連ファイルの修正 #30

--- app/controllers/brand_admin/mypages_controller.rb
+++ app/controllers/brand_admin/mypages_controller.rb
@@ -1,5 +1,5 @@
-class BrandAdmin::MypageController < ApplicationController
-  def index
+class BrandAdmin::MypagesController < ApplicationController
+  def show
     @user = current_user
     @posts = Post.includes(:user).where(user_id: current_user.id).order(created_at: :desc)
   end
