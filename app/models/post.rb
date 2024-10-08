f3ecf571b15f8c5fd08cbc8b42a46dc6de5694e5
takeda-chiaki-53b <toylgmop@gmail.com>
2024-10-08 16:24:53 +0900

    postモデルにenumを追加 #114

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :brand_admin, class_name: "User", optional: true
   has_many :products
+
+  # 投稿のステータス（下書き,公開,非公開）
+  enum status: { draft: 0, published: 1, unpublished: 2 }
 end
