01c29bd87832e6f73f992ac186c04e421727e38e
takeda-chiaki-53b <toylgmop@gmail.com>
2024-10-11 01:05:57 +0900

    ブランドマイページに投稿別のタブを作成、タブの切り替え機能を追加 #117

---
+++ app/javascript/controllers/tabber_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["tab_menu", "content"]
+
+  tab_menuClick(event) {
+    event.preventDefault() // デフォルトの動作(ページのリロードやスクロール位置のリセット)を防ぐ
+    const tab_menus = this.tab_menuTargets // メニューバー全体の要素
+    const current = event.currentTarget // クリックしたメニューバーの要素（現在地）
+    const currentIndex = tab_menus.indexOf(current) // クリックしたメニューバーのインデックス番号
+    const contents = this.contentTargets // コンテンツ全体の要素
+
+    // 初期化（全てのタブを非アクティブにし、コンテンツを隠す）
+    tab_menus.forEach((tab_menu, index) => {
+      tab_menu.classList.remove("is-active", "active-tab")
+      tab_menu.classList.add("not-active")
+      tab_menu.classList.remove("text-blue-600", "border-blue-600")
+      tab_menu.classList.add("text-gray-500", "border-transparent")
+      contents[index].classList.add("hidden")
+    })
+
+    // クリックしたタブをアクティブにし、対応するコンテンツを表示する
+    current.classList.remove("not-active")
+    current.classList.add("is-active", "active-tab")
+    current.classList.add("text-blue-600", "border-blue-600")
+    current.classList.remove("text-gray-500", "border-transparent")
+    contents[currentIndex].classList.remove("hidden") // クリックした要素のコンテンツを表示する
+
+    // ホバーのスタイルを選択時に無効にする
+    tab_menus.forEach(tab_menu => {
+      if (!tab_menu.classList.contains("is-active")) {
+        tab_menu.classList.add("hover:text-gray-600", "hover:border-gray-300")
+      }
+    })
+  }
+}
