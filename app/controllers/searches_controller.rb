e20dd3e70752cda93d21a5cf9788cf2d5894eb07
takeda-chiaki-53b <toylgmop@gmail.com>
2024-10-20 20:35:19 +0900

    コントローラーでブランド管理者のユーザーを取得 #37

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,6 @@ class SearchesController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
   def index
+    @brand_admins = User.brand_admins # スコープでブランド管理者のユーザーを取得
   end
 end
