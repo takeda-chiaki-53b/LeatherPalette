8c2f854655047fbea8fcaea3610ffe379f7666f4
takeda-chiaki-53b <toylgmop@gmail.com>
2024-10-26 20:58:41 +0900

    ユーザー名にユニーク制約を追加 #196

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_12_115859) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_26_112219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_12_115859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "favorites", "posts"
