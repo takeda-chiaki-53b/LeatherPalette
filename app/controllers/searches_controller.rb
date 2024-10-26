3aeec662193ce5bec2d2567af82badeaf99df6a5
takeda-chiaki-53b <toylgmop@gmail.com>
2024-10-26 20:09:51 +0900

    コントローラーのパラメーター、検索処理の修正 #196

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,21 +6,26 @@ class SearchesController < ApplicationController
   end
 
   def result
-    search_brand_id = search_params[:user_id]
+    brand_name = search_params[:brand_name]
     used_year = search_params[:used_year]
 
     # ブランド検索
-    @posts = Post.published.brand_post_search(search_brand_id).includes(:user).order(created_at: :desc)
+    # パラメーターで受け取ったブランド名と一致する、brand_admin権限を持つユーザーを探す。
+    brand_admin = User.brand_admins.find_by(name: brand_name)
+    # 上記で一致するユーザーが存在した場合、投稿の検索
+    if brand_admin.present?
+      @posts = Post.brand_post_search(brand_admin.id).includes(:user).order(created_at: :desc)
+    end
 
     # 使用年数検索
     if used_year.present?
-      @posts = Post.used_year_post_search(used_year) # スコープを使って絞り込み
+      @posts = Post.used_year_post_search(used_year).order(created_at: :desc) # スコープを使って絞り込み
     end
   end
 
   private
 
   def search_params
-    params.require(:post).permit(:user_id, :used_year)
+    params.require(:post).permit(:brand_name, :used_year)
   end
 end
