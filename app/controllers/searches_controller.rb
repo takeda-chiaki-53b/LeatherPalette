20c216e936025c285ffcb8ff15e88067a2b6611b
takeda-chiaki-53b <toylgmop@gmail.com>
2024-10-31 00:28:15 +0900

    コントローラーで複数条件の絞り込み検索の処理を追加＆関連箇所調整 #130

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,17 +9,23 @@ class SearchesController < ApplicationController
     brand_name = search_params[:brand_name]
     used_year = search_params[:used_year]
 
-    # ブランド検索
     # パラメーターで受け取ったブランド名と一致する、brand_admin権限を持つユーザーを探す。
     brand_admin = User.brand_admins.find_by(name: brand_name)
-    # 上記で一致するユーザーが存在した場合、投稿の検索
-    if brand_admin.present?
-      @posts = Post.brand_post_search(brand_admin.id).includes(:user).order(created_at: :desc)
-    end
 
-    # 使用年数検索
-    if used_year.present?
-      @posts = Post.used_year_post_search(used_year).order(created_at: :desc) # スコープを使って絞り込み
+    # 投稿の初期値を全て取得
+    @posts = Post.includes(:user).order(created_at: :desc)
+
+    # 複数の条件がある場合の絞り込み
+    if brand_admin.present? && used_year.present?
+      @posts = @posts.brand_post_search(brand_admin.id).used_year_post_search(used_year) # スコープを活用
+
+    # ブランド名のみパラメーターが存在した場合
+    elsif brand_admin.present?
+      @posts = @posts.brand_post_search(brand_admin.id)
+
+    # 使用年数のみパラメーターが存在した場合
+    elsif used_year.present?
+      @posts = @posts.used_year_post_search(used_year)
     end
   end
 
