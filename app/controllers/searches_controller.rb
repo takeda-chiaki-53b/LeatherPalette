2198bc610afbbc35ebd7e46c468843fb682f49dc
takeda-chiaki-53b <toylgmop@gmail.com>
2024-11-04 23:05:38 +0900

    searchesコントローラーのresultアクションを修正 #208

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,10 +8,12 @@ class SearchesController < ApplicationController
   def result
     brand_name = search_params[:brand_name]
     used_year = search_params[:used_year]
+    color = search_params[:color]
 
     # 選択されたパラメータをインスタンス変数にセット(検索結果に条件を表示させるため)
     @selected_brand_name = brand_name
     @selected_used_year = used_year
+    @selected_color = color
 
     # パラメーターで受け取ったブランド名と一致する、brand_admin権限を持つユーザーを探す。
     brand_admin = User.brand_admins.find_by(name: brand_name)
@@ -19,23 +21,15 @@ class SearchesController < ApplicationController
     # 投稿の初期値を全て取得
     @posts = Post.includes(:user).order(created_at: :desc)
 
-    # 複数の条件がある場合の絞り込み
-    if brand_admin.present? && used_year.present?
-      @posts = @posts.brand_post_search(brand_admin.id).used_year_post_search(used_year) # スコープを活用
-
-    # ブランド名のみパラメーターが存在した場合
-    elsif brand_admin.present?
-      @posts = @posts.brand_post_search(brand_admin.id)
-
-    # 使用年数のみパラメーターが存在した場合
-    elsif used_year.present?
-      @posts = @posts.used_year_post_search(used_year)
-    end
+  # 条件に基づいて絞り込みを行う
+  @posts = @posts.brand_post_search(brand_admin.id) if brand_admin.present?
+  @posts = @posts.used_year_post_search(used_year) if used_year.present?
+  @posts = @posts.color_post_search(color) if color.present?
   end
 
   private
 
   def search_params
-    params.permit(:brand_name, :used_year)
+    params.permit(:brand_name, :used_year, :color)
   end
 end
