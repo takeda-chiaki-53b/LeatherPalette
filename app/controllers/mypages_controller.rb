fc17045a32173cf03fd9af97a6858ca3f83be661
takeda-chiaki-53b <toylgmop@gmail.com>
2024-11-05 22:19:12 +0900

    一般用のmypageコントローラーの作成、表示アクション定義 #132

---
+++ app/controllers/mypages_controller.rb
@@ -0,0 +1,8 @@
+class MypagesController < ApplicationController
+  def show
+    @user = current_user
+    @posts_published = @user.posts.published.order(created_at: :desc)
+    @posts_unpublished = @user.posts.unpublished.order(created_at: :desc)
+    @posts_draft = @user.posts.draft.order(created_at: :desc)
+  end
+end
