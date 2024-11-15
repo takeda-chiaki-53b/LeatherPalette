e44fc06575b59ba0845f59a48c1b279575a72574
takeda-chiaki-53b <toylgmop@gmail.com>
2024-11-15 22:38:28 +0900

    SorceryExternalモジュールの導入、Authenticationテーブルの作成 #140

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_09_074401) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_15_132107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
