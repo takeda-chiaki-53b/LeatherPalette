33818c07b3628637a9b7cb8704dc8449236e927f
takeda-chiaki-53b <toylgmop@gmail.com>
2024-12-01 19:07:46 +0900

    PWA対応 #152

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@
 //   const { title, options } = await event.data.json()
 //   event.waitUntil(self.registration.showNotification(title, options))
 // })
-// 
+//
 // self.addEventListener("notificationclick", function(event) {
 //   event.notification.close()
 //   event.waitUntil(
@@ -12,15 +12,34 @@
 //       for (let i = 0; i < clientList.length; i++) {
 //         let client = clientList[i]
 //         let clientPath = (new URL(client.url)).pathname
-// 
+//
 //         if (clientPath == event.notification.data.path && "focus" in client) {
 //           return client.focus()
 //         }
 //       }
-// 
+//
 //       if (clients.openWindow) {
 //         return clients.openWindow(event.notification.data.path)
 //       }
 //     })
 //   )
 // })
+
+const CACHE_NAME = 'example_cache';
+const urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
