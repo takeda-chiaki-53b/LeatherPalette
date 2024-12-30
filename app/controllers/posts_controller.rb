6bf6de858257beac5b8a8b5d3a2aa34233d712de
takeda-chiaki-53b <toylgmop@gmail.com>
2024-12-30 20:41:25 +0900

    ページネーションを表示させる設定 #292

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :set_products, only: %i[new create edit update]
 
   def index
-    @posts = Post.published.includes(:user).order(created_at: :desc)
+    @posts = Post.published.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def new
