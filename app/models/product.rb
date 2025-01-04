1d2505a3e328b608b40b7b7c6a15931044b934b5
takeda-chiaki-53b <toylgmop@gmail.com>
2025-01-05 08:20:04 +0900

    product_imageの不要なバリデーションの削除とデフォルト画像の設定 #157

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
   mount_uploader :product_image, ProductImageUploader
 
   validates :product_name, presence: true, length: { maximum: 30 }
-  validates :product_image, presence: true
 
   # アソシエーション
   belongs_to :user
