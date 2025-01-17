7155d834085d25384452393663d25e9b45706358
takeda-chiaki-53b <toylgmop@gmail.com>
2025-01-18 01:51:31 +0900

    ボタンやテキストカラーの統一 #318

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'darkgray':'#333333',
-        'beige':'#dbd1c7',
-        'mocha':'#847366',
-        'darkbrown':'#5f534a',
-        'orange':'#d69b71',
-        'natural':'#f3eae1',
+        'darkgray':'#333333', // topアプリ名
+        'beige':'#dbd1c7', // 背景
+        'mocha':'#938073', // topアプリ名の上のフレーズ
+        'darkbrown':'#5f534a', // テキスト全版
+        'light_orange':'#E6CCB585', // top見てみるボタン
+        'orange':'#fdba74', // top見てみるボタン
+        'natural':'#f3eae1', // ヘッダーロゴ枠、ボタンテキスト
+        'gray1':'#e5e5e5', // 削除ボタン
+        'gray2':'#a3a3a3', // 削除ボタンホバー
       },
       fontfamily: {
         'noto':['Noto Serif JP', 'serif'],
